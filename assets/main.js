668f3f14afa6dd641b3bcae8b93dd5c98ec3e970
giuseppe <placidagiuseppe@gmail.com>
2021-04-12 18:53:12 +0200

    milestone 3 quasi  completata✅

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,7 @@ var app = new Vue({
   el: '#root',
   data: {
     films: [],
+    serietv:[],
     search: "",
   },
 
@@ -10,10 +11,21 @@ var app = new Vue({
     add:function(){
       axios.get(`https://api.themoviedb.org/3/search/movie?api_key=a2391ef42653febeddc812031ec86499&query=${this.search}`)
            .then((response) =>{
-             console.log(response.data.results);
+
+             // const results =response.data.results;
+             // this.films =this.films.concat(results);
+             // console.log(this.films);
              this.films = response.data.results;
              console.log(this.films);
            });
+      axios.get(`https://api.themoviedb.org/3/search/tv?api_key=a2391ef42653febeddc812031ec86499&query=${this.search}`)
+                .then((response) =>{
+                
+                  // const results = response.data.results;
+                  // this.films = this.films.concat(results);
+                  // console.log(this.films);
+                  this.serietv = response.data.results;
+                });
     }
   }
 
