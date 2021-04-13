fad50c801cdc1186cea8dc4378bad386fabd1442
giuseppe <placidagiuseppe@gmail.com>
2021-04-13 12:32:06 +0200

    milestone 3 completata

--- assets/main.js
+++ assets/main.js
@@ -3,30 +3,50 @@ var app = new Vue({
   el: '#root',
   data: {
     films: [],
-    serietv:[],
+    // serietv:[],
     search: "",
+    uri:"https://api.themoviedb.org/3",
+    api_key:"a2391ef42653febeddc812031ec86499",
+    language:"it",
   },
 
   methods:{
     add:function(){
-      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=a2391ef42653febeddc812031ec86499&query=${this.search}`)
+      axios.get(`${this.uri}/search/movie?api_key=${this.api_key}&query=${this.search}&language=${this.language}`)
            .then((response) =>{
-
-             // const results =response.data.results;
+             const results = response.data.results;
              // this.films =this.films.concat(results);
+             //anche questa this.films = response.data.results;
              // console.log(this.films);
-             this.films = response.data.results;
+             this.films = [...this.films, ...results ];
              console.log(this.films);
            });
-      axios.get(`https://api.themoviedb.org/3/search/tv?api_key=a2391ef42653febeddc812031ec86499&query=${this.search}`)
+      axios.get(`${this.uri}/search/tv?api_key=${this.api_key}&query=${this.search}&language=${this.language}`)
                 .then((response) =>{
-                
-                  // const results = response.data.results;
-                  // this.films = this.films.concat(results);
-                  // console.log(this.films);
-                  this.serietv = response.data.results;
+                  const results = response.data.results;
+                  // this.films = this.films.concat(results)  concateno flm con serie tv;
+                  // console.log(this.films); this.serietv = response.data.results;
+                  this.films = [...this.films, ...results ];
                 });
+    },
+    get_title: function(obj){
+       if (obj.title) {
+          return obj.title;
+       }else {
+         return obj.name;
+       }
+    },
+    get_original_title:function(obj){
+      if (obj.original_title) {
+         return obj.original_title;
+      }else {
+        return obj.original_name;
+      }
+    },
+    vote:function(voto){
+      return Math.round(voto / 2)
     }
+
   }
 
 
